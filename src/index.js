7d8bfbaaa69d6623464edc74fae0af0c98354f68
Valery <gooldg9@gmail.com>
2023-06-15 02:30:23 +0700

    обновления

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import AuthProvider from './contexts/AuthContexts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>,
 );
