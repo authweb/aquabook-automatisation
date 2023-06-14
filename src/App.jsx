c204934068b8b7c3a383fab061b9bef0f6d7ecab
Valery <gooldg9@gmail.com>
2023-06-15 05:13:46 +0700

    обновление роутов

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './scss/styles.scss'; // добавляем импорт стилей
-import { HomePage, Appointment, AuthPage, Profile, PersonalInfo, CarInfoPage } from './pages';
+import {
+  HomePage,
+  Appointment,
+  AuthPage,
+  Profile,
+  PersonalInfo,
+  CarInfoPage,
+  Dashboard,
+} from './pages';
 import { UserContext } from './contexts/UserContexts';
 import AuthProvider from './contexts/AuthContexts';
 
 const App = () => {
   //   const { user } = useContext(AuthContext);
-  const [clients, setUser] = useState(null);
+  const [clients, setClients] = useState(null);
   //   console.log(user.first_name);
   return (
     <div className="App">
@@ -15,14 +23,18 @@ const App = () => {
         {/* <h1>User: {user && user.first_name}</h1> */}
         {/* first_name={user.first_name} */}
         <AuthProvider>
-          <UserContext.Provider value={{ clients, setUser }}>
+          <UserContext.Provider value={{ clients, setClients }}>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/appointment" element={<Appointment />} />
               <Route path="/auth" element={<AuthPage />} />
-              <Route path="/profile/:clients_id" element={<Profile />} />
-              <Route path="/profile/:clients_id/personal-info" element={<PersonalInfo />} />
-              <Route path="/profile/:clients_id/personal-info/car-info" element={<CarInfoPage />} />
+              <Route path="profile/:clients_id">
+                <Route index element={<Profile />} />
+                <Route path="personal-info" element={<PersonalInfo />}>
+                  <Route path="car-info" element={<CarInfoPage />} />
+                </Route>
+              </Route>
+              <Route path="/admin/dashboard/" element={<Dashboard />} />
             </Routes>
           </UserContext.Provider>
         </AuthProvider>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { default as Appointment } from './AppointmentPage/AppointmentPage';
 export { default as Profile } from './Profile/Profile';
 export { default as PersonalInfo } from './Profile/PersonalInfo';
 export { default as CarInfoPage } from './CarInfoPage/CarInfoPage';
+export { default as Dashboard } from './Dashboard/Dashboard';
