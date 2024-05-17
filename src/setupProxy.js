dae83ca8c6b17c913aa591bcd609c530829bf710
Valery Gololobov <43993043+authweb@users.noreply.github.com>
2024-05-17 11:02:06 +0700

    обновление

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,6 +6,21 @@ module.exports = function (app) {
 		createProxyMiddleware({
 			target: "http://aqua-book:3306",
 			changeOrigin: true,
+			onProxyReq: (proxyReq, req) => {
+				if (!req.body || !Object.keys(req.body).length) {
+					return;
+				}
+
+				const contentType = proxyReq.getHeader("Content-Type");
+
+				if (contentType.includes("application/json")) {
+					const bodyData = JSON.stringify(req.body);
+					// Set the content length header for the proxy request
+					proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+					// Write the JSON stringified body to the proxy request
+					proxyReq.write(bodyData);
+				}
+			},
 		}),
 	);
 };
