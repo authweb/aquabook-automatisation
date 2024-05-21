3a790615c2b3e2ae1e781f488eb689b2403fa4c8
Valery Gololobov <43993043+authweb@users.noreply.github.com>
2024-05-22 00:10:19 +0700

    обновление

--- src/api/addAppointments.js
+++ src/api/addAppointments.js
@@ -1,6 +1,7 @@
+// addAppointments.js
 const express = require("express");
 const router = express.Router();
-const db = require("../config/dbConnect");
+const pool = require("../config/dbConnect");
 
 router.post("/appointments", async (req, res) => {
 	const {
@@ -25,18 +26,23 @@ router.post("/appointments", async (req, res) => {
 		return res.status(400).json({ message: "Все поля должны быть заполнены" });
 	}
 
+	let connection;
+
 	try {
+		// Получаем соединение из пула
+		connection = await pool.getConnection();
+
 		// Начинаем транзакцию
-		await db.beginTransaction();
+		await connection.beginTransaction();
 
 		// Добавляем новую запись в таблицу appointments
-		const [appointmentResult] = await db.execute(
+		const [appointmentResult] = await connection.execute(
 			"INSERT INTO appointments (start, end, text, clients_id, totalCost) VALUES (?, ?, ?, ?, ?)",
 			[start, end, text, clients_id, totalCost],
 		);
 
 		if (appointmentResult.affectedRows === 0) {
-			await db.rollback();
+			await connection.rollback();
 			return res.status(400).json({ message: "Failed to create appointment" });
 		}
 
@@ -44,14 +50,14 @@ router.post("/appointments", async (req, res) => {
 
 		// Добавляем записи в таблицу service_employee_map
 		for (const { service_id, employee_id } of serviceEmployeeMap) {
-			await db.execute(
+			await connection.execute(
 				"INSERT INTO service_employee_map (appointment_id, service_id, employee_id) VALUES (?, ?, ?)",
 				[appointmentId, service_id, employee_id],
 			);
 		}
 
 		// Фиксируем транзакцию
-		await db.commit();
+		await connection.commit();
 
 		res.status(201).json({
 			message: "Appointment created successfully",
@@ -66,13 +72,17 @@ router.post("/appointments", async (req, res) => {
 			},
 		});
 	} catch (error) {
-		// Откат транзакции в случае ошибки
-		await db.rollback();
+		if (connection) {
+			// Откат транзакции в случае ошибки
+			await connection.rollback();
+		}
 		console.error(error);
 		res.status(500).json({ message: "Server error" });
 	} finally {
-		// Закрываем соединение
-		await db.end();
+		if (connection) {
+			// Освобождаем соединение
+			connection.release();
+		}
 	}
 });
 
