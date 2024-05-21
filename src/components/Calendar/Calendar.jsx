c185ed0dac2683c5759765e64b79b6b7451a5423
Valery Gololobov <43993043+authweb@users.noreply.github.com>
2024-05-22 01:07:44 +0700

    обновление

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -12,47 +12,11 @@ import { AppointmentDetails } from "../";
 const CalendarDay = () => {
 	const navigate = useNavigate();
 	const location = useLocation();
-	const { selectedDate, setSelectedEmployeeId, setCurrentEventId } =
+	const { setSelectedEmployeeId, setCurrentEventId } =
 		useContext(CalendarContext);
-	const [selectedEvent, setSelectedEvent] = useState(null);
-	const { today, rangeStart, setToday, setRangeStart } = useDateHandler();
-	const [prevConfig, setPrevConfig] = useState({});
-
 	const [events, setEvents] = useState([]);
-	const [date, setDate] = useState(dayjs());
-	const [timeRange, setTimeRange] = useState([
-		dayjs().startOf("hour"),
-		dayjs().endOf("hour"),
-	]);
-	const [categories, setCategories] = useState([]);
-	const [services, setServices] = useState({});
-
-	useEffect(() => {
-		const startDate =
-			selectedDate instanceof dayjs
-				? selectedDate.format("YYYY-MM-DD")
-				: dayjs(selectedDate).format("YYYY-MM-DD");
-
-		setPrevConfig(prevConfig => ({
-			...prevConfig,
-			startDate: startDate,
-		}));
-	}, [selectedDate, setPrevConfig]);
-
-	useEffect(() => {
-		console.log("Date state updated:", date);
-	}, [date]);
-
-	useEffect(() => {
-		console.log("Time range state updated:", timeRange);
-	}, [timeRange]);
-
-	useEffect(() => {
-		setConfig(prevConfig => ({
-			...prevConfig,
-			startDate: dayjs(selectedDate).format("YYYY-MM-DD"),
-		}));
-	}, [selectedDate]); // Обновлять конфигурацию каждый раз, когда selectedDate меняется
+	const [employees, setEmployees] = useState([]);
+	const { selectedDate } = useContext(CalendarContext);
 
 	useEffect(() => {
 		async function fetchData() {
@@ -62,37 +26,20 @@ const CalendarDay = () => {
 					axios.get("https://api.aqua-book.ru/api/employees"),
 				]);
 
+				const { appointments } = appointmentsResponse.data;
 				const { employees } = employeesResponse.data;
 
-				// Создаем карту соответствия имени сотрудника и его id
-				const employeeMap = new Map();
-				employees.forEach(employee => {
-					employeeMap.set(employee.id, employee.first_name);
+				setEmployees(employees);
+
+				const mappedAppointments = appointments.map(appt => {
+					return {
+						...appt,
+						start: new Date(appt.start),
+						end: new Date(appt.end),
+					};
 				});
 
-				// Преобразуем данные о назначениях в массив событий для календаря
-				const events = appointmentsResponse.data.appointments
-					.map(appt => {
-						const employeeId = appt.employee_id;
-						const employeeName = employeeMap.get(employeeId);
-						if (!employeeName) {
-							console.error(`Employee name for ID '${employeeId}' not found.`);
-							return null;
-						}
-						return {
-							id: appt.id.toString(),
-							start: appt.start,
-							end: appt.end,
-							text: appt.text,
-							resource: employeeId.toString(),
-							backColor: "#someColor",
-							employeeName: employeeName,
-						};
-					})
-					.filter(event => event !== null);
-
-				setEvents(events);
-				console.log(appointmentsResponse);
+				setEvents(mappedAppointments);
 			} catch (error) {
 				console.error("Error fetching data:", error);
 			}
@@ -101,59 +48,37 @@ const CalendarDay = () => {
 		fetchData();
 	}, []);
 
+	const handleTimeRangeSelected = args => {
+		const selectedStart = args.start.value;
+		const selectedEnd = args.end.value;
+		navigate(
+			`/dashboard/appointments/add?start=${selectedStart}&end=${selectedEnd}`,
+		);
+	};
+
 	const handleEventClick = args => {
 		const eventId = args.e.data.id;
 		setCurrentEventId(eventId);
 		navigate(`/dashboard/appointments/${eventId}`);
 	};
 
-	const [config, setConfig] = useState({
-		viewType: "Resources",
-		startDate: (today
-			? dayjs(today)
-			: selectedDate
-			? dayjs(selectedDate)
-			: dayjs()
-		).format("YYYY-MM-DD"),
-		columns: [],
-		heightSpec: "BusinessHoursNoScroll",
-		theme: "eb-calendar",
-		businessBeginsHour: 9,
-		businessEndsHour: 21,
-		onEventClick: handleEventClick,
-		timeFormat: "Clock24Hours",
-		showNonBusiness: false,
-		timeHeaders: [
-			{ groupBy: "Day", format: "dddd MMMM yyyy" },
-			{ groupBy: "Hour", format: "h tt" },
-		],
-		cellDuration: 15,
-		timeRangeSelectedHandling: "Enabled",
-	});
-
-	const handleTimeRangeSelected = args => {
-		console.log(args);
-		setSelectedEmployeeId(args.resource);
-		const selectedStart = args.resource.start
-			? dayjs(args.resource.start.value)
-			: null;
-		const selectedEnd = args.end ? dayjs(args.end.value) : null;
-		navigate(
-			`${location.pathname}/add${location.search}&start=${
-				selectedStart ? selectedStart.format("YYYY-MM-DDTHH:mm:ss") : ""
-			}&end=${selectedEnd ? selectedEnd.format("YYYY-MM-DDTHH:mm:ss") : ""}`,
-		);
-	};
 	return (
 		<>
 			<DayPilotCalendar
-				className='eb-calendar__columns'
-				startDate={config.startDate}
+				startDate={selectedDate}
 				events={events}
-				{...config}
+				timeRangeSelectedHandling='Enabled'
 				onTimeRangeSelected={handleTimeRangeSelected}
+				onEventClick={handleEventClick}
+				businessBeginsHour={9}
+				businessEndsHour={21}
+				timeHeaders={[
+					{ groupBy: "Day", format: "dddd MMMM yyyy" },
+					{ groupBy: "Hour", format: "h tt" },
+				]}
+				cellDuration={15}
+				viewType='Day'
 			/>
-			{selectedEvent && <AppointmentDetails event={selectedEvent} />}
 		</>
 	);
 };
