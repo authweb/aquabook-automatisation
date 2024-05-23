b3a5b5f1b0977cb2e572eecff60c5c65dee4c5f7
Valery Gololobov <43993043+authweb@users.noreply.github.com>
2024-05-24 01:04:21 +0700

    Добавление записи

--- src/api/addAppointments.js
+++ src/api/addAppointments.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const db = require("../config/dbConnect");
 
 router.post("/appointments", async (req, res) => {
+	console.log("Received POST request to /api/appointments");
+	console.log("Request body:", req.body);
+
 	const {
 		start,
 		end,
@@ -15,22 +18,19 @@ router.post("/appointments", async (req, res) => {
 
 	const errors = [];
 
+	// Проверяем наличие обязательных полей в запросе
 	if (start === undefined) {
 		errors.push("start is missing");
 	}
-
 	if (end === undefined) {
 		errors.push("end is missing");
 	}
-
 	if (text === undefined) {
 		errors.push("text is missing");
 	}
-
 	if (typeof totalCost !== "number") {
 		errors.push("totalCost should be a number");
 	}
-
 	if (typeof clients_id !== "number") {
 		errors.push("clients_id should be a number");
 	}
@@ -43,12 +43,14 @@ router.post("/appointments", async (req, res) => {
 	try {
 		// Добавляем новую запись с новыми столбцами
 		const [appointmentResult] = await db.execute(
-			"INSERT INTO appointments (start, end, selectedServices, serviceEmployeeMap, text, clients_id, totalCost) VALUES (?, ?, ?, ?, ?, ?, ?)",
+			`INSERT INTO appointments 
+             (start, end, selectedServices, serviceEmployeeMap, text, clients_id, totalCost) 
+             VALUES (?, ?, ?, ?, ?, ?, ?)`,
 			[
 				start,
 				end,
-				selectedServices,
-				serviceEmployeeMap,
+				JSON.stringify(selectedServices), // Преобразуем массив в строку JSON
+				JSON.stringify(serviceEmployeeMap), // Преобразуем объект Map в строку JSON
 				text,
 				clients_id,
 				totalCost,
@@ -74,7 +76,7 @@ router.post("/appointments", async (req, res) => {
 			},
 		});
 	} catch (error) {
-		console.error(error);
+		console.error("Error processing POST request to /api/appointments:", error);
 		res.status(500).json({ message: "Server error" });
 	}
 });
